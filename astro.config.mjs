c5d436970ed36d6fce07d5e35bd6f697ec819a42
arkeonagency <btadele009@gmail.com>
2025-12-22 11:32:41 +0000

    v23

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,23 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import partytown from '@astrojs/partytown'; // <--- Import
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-  site: 'https://arkeon-studio.vercel.app',
+  site: 'https://arkeon-studio.vercel.app', // Update this if you have a custom domain later
+  
   integrations: [
     react(),
-    tailwind({ applyBaseStyles: false }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
     sitemap(),
-    partytown({ // <--- Add config
+    partytown({
       config: {
         forward: ["dataLayer.push"],
       },
     }),
+    // Keystatic removed from here
   ],
   output: 'static',
 });
